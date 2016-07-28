09be58f3ce93d7434be3e70434a06ec811dad0b0
onbjerg <hi@notbjerg.me>
2016-07-28 23:53:15 +0200

    Add fetch method

---
+++ examples/fetch.js
@@ -0,0 +1,5 @@
+import { fetch } from '../src'
+
+fetch(-33.870958946626885, 151.21347069740295)
+  .then(({ status, pokemon }) => console.log(status, pokemon))
+  .catch((err) => console.error(err))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,54 @@
 import axios from 'axios'
 import promiseRetry from 'promise-retry'
 
-export default (lat, lng, filter = null) => {
-  const baseURI = 'https://pokevision.com/map'
+const baseURI = 'https://pokevision.com/map'
 
+/**
+ * Fetches map data. Does _not_ scan the area, only fetches readily available
+ * data from the Pokevision API (i.e. data from recent scans).
+ *
+ * @param  {Number} lat
+ * @param  {Number} lng
+ * @param  {Array} filter = null
+ * @param  {String} jobId = null
+ * @return {Promise}
+ */
+export const fetch = (lat, lng, filter = null, jobId = null) => {
+  return axios.get(`${baseURI}/data/${lat}/${lng}/${jobId}`).then((res) => {
+    let {
+      status,
+      jobStatus,
+      pokemon
+    } = res.data
+
+    // If the job is still running we will throw this error
+    // which will prompt the promise to retry
+    if (jobStatus && jobStatus === 'in_progress') {
+      throw new Error('Job is in progress, retrying')
+    }
+
+    // Filter the Pokémon by their Pokedex ID if a filter is specified
+    if (filter !== null) {
+      pokemon = pokemon.filter((p) => filter.indexOf(p.pokemonId) !== -1)
+    }
+
+    return {
+      status,
+      pokemon
+    }
+  })
+}
+
+/**
+ * Scans the area for new Pokémon and fetches the map data when the scan
+ * is complete.
+ *
+ * @param  {Number} lat
+ * @param  {Number} lng
+ * @param  {Array} filter = null
+ * @return {Promise}
+ */
+const scan = (lat, lng, filter = null) => {
   return axios.get(`${baseURI}/scan/${lat}/${lng}`).then((res) => {
     let {
       status,
@@ -26,29 +71,8 @@ export default (lat, lng, filter = null) => {
 
     // Fetch the map data from the job we created
     return promiseRetry((retry, tries) => {
-      return axios.get(`${baseURI}/data/${lat}/${lng}/${jobId}`).then((res) => {
-        let {
-          status,
-          jobStatus,
-          pokemon
-        } = res.data
-
-        // If the job is still running we will throw this error
-        // which will prompt the promise to retry
-        if (jobStatus && jobStatus === 'in_progress') {
-          throw new Error('Job is in progress, retrying')
-        }
-
-        // Filter the Pokémon by their Pokedex ID if a filter is specified
-        if (filter !== null) {
-          pokemon = pokemon.filter((p) => filter.indexOf(p.pokemonId) !== -1)
-        }
-
-        return {
-          status,
-          pokemon
-        }
-      }).catch((err) => retry(err))
+      return fetch(lat, lng, filter, jobId).catch((err) => retry(err))
     })
   })
 }
+export default scan
